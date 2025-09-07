cb770c45bc0b1420b361963a5d68ef4c2bab25b8
SofiaLee07 <143029249+SofiaLee07@users.noreply.github.com>
2025-09-07 20:59:41 -0300

    Update arcondicionado.h

--- Exercicios_Lab1/ex06/arcondicionado.h
+++ Exercicios_Lab1/ex06/arcondicionado.h
@@ -1,4 +1,5 @@
-/* Implemente a abstração de um equipamento de ar condicionado, com os seguintes requisitos:
+/* Exercicio 6
+Implemente a abstração de um equipamento de ar condicionado, com os seguintes requisitos:
 
 * O condicionador possui 10 potências diferentes.
 * Cada unidade da potência do condicionador diminui a temperatura do ambiente em 1.8° C.
@@ -6,6 +7,7 @@
   zero graus de variação quando desligado e dezoito graus de variação quando ligado na potência máxima.
 * Através de um sensor, o condicionador é informado da temperatura externa. Dada essa temperatura 
   e a potência selecionada, o condicionador calcula e retorna a temperatura do ambiente.
+  
 No programa principal, crie dois condicionadores. Informe duas temperaturas externas diferentes para 
 cada um (ex: 25° C e 31° C), ajuste o segundo em potência máxima (10) e o primeiro em potência média (5).
  Finalmente, exiba a temperatura resultante de cada ambiente.*/
@@ -37,3 +39,4 @@ public:
                   << std::endl;
     }
 };
+
