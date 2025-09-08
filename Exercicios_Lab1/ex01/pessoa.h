33b561911bd0c8dd0845425be855628402b879de
SofiaLee07 <143029249+SofiaLee07@users.noreply.github.com>
2025-09-07 21:01:48 -0300

    Update pessoa.h

--- Exercicios_Lab1/ex01/pessoa.h
+++ Exercicios_Lab1/ex01/pessoa.h
@@ -1,5 +1,6 @@
 /*Exercício 01
-Crie uma classe Pessoa para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os métodos públicos necessários para acessar e modificar os atributos desta classe e crie um método para imprimir os dados da pessoa.
+Crie uma classe Pessoa para representar uma pessoa, com os atributos privados de nome, idade e altura. Crie os métodos públicos 
+necessários para acessar e modificar os atributos desta classe e crie um método para imprimir os dados da pessoa.
 
 Implemente um programa de teste que utilize toda a interface da classe criada.*/
 
@@ -28,3 +29,4 @@ public:
 };
 
 #endif // PESSOA_H
+
